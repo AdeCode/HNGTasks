2e87fdf58ce7b371b8966494cc9ec103508eb0af
heybeecode <habeeb@springcash.co>
2023-09-10 17:48:57 +0100

    updated route path

--- server.js
+++ server.js
@@ -22,11 +22,21 @@ app.use((req, res, next)=>{
 })
 
 //routes
-app.use('/api', infoRoutes)
+//app.use('/api', infoRoutes)
 
-app.get('/', ()=>{
-    return res.json({
-        message: 'Welcome to HNG'
+const date = new Date() 
+const day = new Date().toLocaleDateString('en-us', { weekday:"long"}) 
+
+app.get('/api', (req,res) => {
+    const {slack_name, track} = req.query
+    return res.status(200).json({
+        slack_name:slack_name,
+        current_day:day,
+        utc_time: date,
+        track: track,
+        github_file_url:'https://github.com/AdeCode/HNGTasks/blob/main/server.js',
+        github_repo_url:'https://github.com/AdeCode/HNGTasks/tree/main',
+        status_code: 200,
     })
 })
 
