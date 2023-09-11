82c7f2d565a64517f3b78285725a9e45fdf56739
heybeecode <habeeb@springcash.co>
2023-09-11 12:25:27 +0100

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const infoRoutes = require('./routes/info')
 var cors = require('cors')
-const moment = require('moment')
+// const moment = require('moment')
 
 //express app
 const app = express()
@@ -25,16 +25,16 @@ app.use((req, res, next)=>{
 //routes
 //app.use('/api', infoRoutes)
 
-const utc_time = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]')
 const day = new Date().toLocaleDateString('en-us', { weekday:"long"}) 
-//console.log(utc_time)
+const date = new Date().toISOString().split('.')[0]+'Z'
+console.log(date)
 
 app.get('/api', (req,res) => {
     const {slack_name, track} = req.query
     return res.status(200).json({
         slack_name:slack_name,
         current_day:day,
-        utc_time: utc_time,
+        utc_time: date,
         track: track,
         github_file_url:'https://github.com/AdeCode/HNGTasks/blob/main/server.js',
         github_repo_url:'https://github.com/AdeCode/HNGTasks/tree/main',
