5c5d8d20531896a5dab0ed8ed1c0c11e00649e31
heybeecode <habeeb@springcash.co>
2023-09-12 12:20:25 +0100

    updated

--- server.js
+++ server.js
@@ -35,12 +35,12 @@ app.get('/api', (req,res) => {
     const {slack_name, track} = req.query
     return res.status(200).json({
         slack_name:slack_name,
+        current_day:day,
+        utc_time: utc_time,
         track: track,
         github_file_url:'https://github.com/AdeCode/HNGTasks/blob/main/server.js',
         github_repo_url:'https://github.com/AdeCode/HNGTasks/tree/main',
         status_code: 200,
-        current_day:day,
-        utc_time: date,
     })
 })
 
